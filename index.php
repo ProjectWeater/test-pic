e774acb5e5429689236dec3a01f5ad072ab82003
ProjectWeater <97742494+u2t-saopao@users.noreply.github.com>
2022-02-22 14:31:51 +0700

    xxx

--- index.php
+++ index.php
@@ -1,20 +1,52 @@
 <?php
-		//Set the Content Type
-		header('Content-type: image/jpeg');
-		// Create Image From Existing File
-		$img = imagecreatefromjpeg('pic.jpg');
-		// Allocate A Color For The Text
-		$white = imagecolorallocate($img, 255, 255, 255);	  
-		// Set Path to Font File
-		$font_path = 'arial.ttf';	  
-		// Set Text to Be Printed On Image
-		$text = "Hello to test";	  
-		// Print Text On Image
-		imagettftext($img, 25, 0, 75, 300, $white, $font_path, $text);  
-		// Send Image to Browser
-		imagejpeg($img);
-		// Clear Memory
-		imagedestroy($img);
-		$quality = 100;
-		imagejpeg($img, "demo.jpg", $quality);
+
+  //select the image on which you would like to write the text
+ 
+ $sourcelocation = ('pic.jpg');
+
+  
+  
+//create the image from existing image
+  
+$image = imagecreatefromjpeg($sourcelocation);
+
+  
+//destination file name where you want to store the new image
+
+  $output = "images/text.jpg";
+
+ 
+ 
+ //declaring the colors
+  
+$white = imagecolorallocate($image,255,255,255);
+  
+$black = imagecolorallocate($image,0,0,0);
+
+ 
+ //font size of the text
+
+  $font_size = 40;
+
+
+ 
+ //rotation of the text
+  
+$rotation = 0;
+
+  
+//font style of the text.we need to give the location of.ttf file
+ 
+ $font = "font\arial.ttf";
+
+ 
+ //text you want to add to the image
+ 
+ $text="Hello";
+$text1 = imagettftext($image,$font_size,$rotation,25, 0, 75, 300,$white,$font,$text);
+
+ 
+ imagejpeg($image,$output);
+
+
 ?>
