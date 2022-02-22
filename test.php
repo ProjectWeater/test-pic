a72dca1509d0698ff8d266d874cc2c5f3b6eed4f
ProjectWeater <97742494+u2t-saopao@users.noreply.github.com>
2022-02-22 11:04:13 +0700

    f
    ff

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+		//Set the Content Type
+		header('Content-type: image/jpeg');
+		// Create Image From Existing File
+		$img = imagecreatefromjpeg('pic.jpg');
+		// Allocate A Color For The Text
+		$white = imagecolorallocate($img, 255, 255, 255);	  
+		// Set Path to Font File
+		$font_path = 'arial.ttf';	  
+		// Set Text to Be Printed On Image
+		$text = "This is a sunset!";	  
+		// Print Text On Image
+		imagettftext($img, 25, 0, 75, 300, $white, $font_path, $text);  
+		// Send Image to Browser
+		imagejpeg($img);
+		// Clear Memory
+		imagedestroy($img);
+		$quality = 100;
+		imagejpeg($img, "demo.jpg", $quality);
+?>
